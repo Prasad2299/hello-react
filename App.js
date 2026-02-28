c4f55fa361b1bc1378dbe1005ef58f03a9120068
Prasad2299 <prasadramphale354@gmail.com>
2026-02-28 19:21:38 +0530

    heading object

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
     // create element in react
     const heading = React.createElement("h1",{id:"heading"},"Hello world from React!")
+    console.log(heading)//object
     //create root in react
     const root =  ReactDOM.createRoot(document.getElementById("root"))
     //rendering heading in react
