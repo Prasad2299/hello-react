7adfafeb417fa882547244d66054fab5834a01d1
Prasad2299 <prasadramphale354@gmail.com>
2026-02-28 19:40:13 +0530

    complex structure for nested element

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-    // create element in react
-    const heading = React.createElement("h1",{id:"heading"},"Hello world from React!")
-    console.log(heading)//object
-    //create root in react
-    const root =  ReactDOM.createRoot(document.getElementById("root"))
-    //rendering heading in react
-    root.render(heading)
+    // // create element in react
+    // const heading = React.createElement("h1",{id:"heading"},"Hello world from React!")
+    // console.log(heading)//object
+    // //create root in react
+    // const root =  ReactDOM.createRoot(document.getElementById("root"))
+    // //rendering heading in react
+    // root.render(heading)
+
+
+    /*
+      <div id="parent">
+        <div id="child">
+          <h1>HEADING</h1>
+        </div>
+      </div>
+    */
+
+// complex structure
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "THIS IS HEADING!"),
+  ),
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
