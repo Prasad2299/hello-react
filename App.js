9eebdb91e0bfb8caa3be1f904d2c62b14a523a25
Prasad2299 <prasadramphale354@gmail.com>
2026-03-16 15:08:13 +0530

    react element vs jsx

--- App.js
+++ App.js
@@ -1,77 +1,12 @@
-// // create element in react
-// const heading = React.createElement("h1",{id:"heading"},"Hello world from React!")
-// console.log(heading)//object
-// //create root in react
-// const root =  ReactDOM.createRoot(document.getElementById("root"))
-// //rendering heading in react
-// root.render(heading)
-
-//     /*
-//       <div id="parent">
-//         <div id="child">
-//           <h1>HEADING</h1>
-//         </div>
-//       </div>
-//     */
-
-// // complex structure
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "THIS IS HEADING!"),
-//   ),
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-
-/*
-      <div id="parent">
-        <div id="child">
-          <h1>HEADING</h1>
-          <h2>heading 2</h2>
-        </div>
-      </div>
-    */
-
-// complex SIBLING structure
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     [React.createElement("h1", {}, "THIS IS HEADING!"),React.createElement("h2",{},"THIS IS HEADING 2")]
-//   ),
-// );
-
-/*
-      <div id="parent">
-        <div id="child">
-          <h1>HEADING</h1>
-          <h2>heading 2</h2>
-        </div>
-      </div>
-    */
-
-// more complex SIBLING structure
-
 import React from "react";
 import ReactDOM from "react-dom/client"
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div",{ id: "child1", key: "child1" }, [
-    React.createElement("h1", {key:"h1"}, "THIS IS HEADING!"),
-    React.createElement("h2", {key:"h2"}, "THIS IS HEADING 2"),
-  ]),
-  React.createElement("div", { id: "child2" ,key:"child2" }, [
-    React.createElement("h1", {key:"h1"}, "THIS IS HEADING!"),
-    React.createElement("h2", {key:"h2"}, "THIS IS HEDING 2"),
-  ]),
-]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading = React.createElement("h1",{id:"heading"},"React Element!")
+console.log(heading);
+
+const jsxHeading = <h1 id="heading">"Pr"</h1>
+
+console.log(jsxHeading)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
