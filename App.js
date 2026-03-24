9b64b877fbb83cb97ee20596af03cf99885796b5
Prasad2299 <prasadramphale354@gmail.com>
2026-03-24 17:30:38 +0530

    added body component

--- App.js
+++ App.js
@@ -18,13 +18,46 @@ const Header = () => {
         <li>Cart</li>
       </ul>
     </div>
- 
-    </div>
+  </div>
 }
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+      <img alt="res-logo" src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/DINEOUT_ALL_RESTAURANTS/IMAGES/RESTAURANT_IMAGE_SERVICE/2025/12/19/8a3cdbf9-91e7-43b1-89a7-c90444fee7ac_image3a8aaa2225c674637a5083fc6e59d66d7.JPG"></img>
+      <h3>Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">SEARCH</div>
+      <div className="res-container">
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+      </div>
+    </div>
+  )
+}
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header></Header>
+      <Body></Body>
     </div>
   )
 }
