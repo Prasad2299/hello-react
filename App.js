ca802141d61bed485d3e55c9f79348adb1f29291
Prasad2299 <prasadramphale354@gmail.com>
2026-03-24 17:59:31 +0530

    added props

--- App.js
+++ App.js
@@ -21,11 +21,12 @@ const Header = () => {
   </div>
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const {resName} = props
   return (
     <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
       <img alt="res-logo" src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/DINEOUT_ALL_RESTAURANTS/IMAGES/RESTAURANT_IMAGE_SERVICE/2025/12/19/8a3cdbf9-91e7-43b1-89a7-c90444fee7ac_image3a8aaa2225c674637a5083fc6e59d66d7.JPG"></img>
-      <h3>Foods</h3>
+      <h3>{resName}</h3>
       <h4>Biryani, North Indian, Asian</h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
@@ -38,16 +39,8 @@ const Body = () => {
     <div className="body">
       <div className="search">SEARCH</div>
       <div className="res-container">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard resName = "Meghana Foods"/>
+        <RestaurantCard resName = "KFC Foods"/>
       </div>
     </div>
   )
