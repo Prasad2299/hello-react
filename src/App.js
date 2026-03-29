1829c4432dd2c47db4e1757b05c4209934ad6b94
Prasad2299 <prasadramphale354@gmail.com>
2026-03-29 23:44:04 +0530

    added contact and error components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter,RouterProvider } from "react-router";
 import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
   return (
@@ -17,11 +19,16 @@ const AppLayout = () => {
 const appRouter = createBrowserRouter([
   {
     path:"/",
-    element:<AppLayout/>
+    element:<AppLayout/>,
+    errorElement: <Error/>
   },
   {
     path:"/about",
     element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
   }
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
