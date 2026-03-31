4df51896b73595c673de6feeb0fde7f41ca4430d
Prasad2299 <prasadramphale354@gmail.com>
2026-03-31 16:30:39 +0530

    added class component with props

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,11 @@
+import UserClass from "./UserClass";
+import User from "./User";
+
 const About = () =>{
-  return <div>
+  return <div >
     <h1>Welcome to About Us</h1>
+    <User name={"Prasad"} location = {"Pune"}></User>
+    <UserClass name={"Prasad class"} location = {"Pune-class"}></UserClass>
   </div>
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name,location}) => {
+  return (
+    <div className="user-card">
+      <h1>Name : {name} </h1>
+      <h2>Location : {location}</h2>
+    </div>
+  )
+}
+
+export default User;
