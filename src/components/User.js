a4bce839076465bb4a21d6c6a575e25aef5bbfc2
Prasad2299 <prasadramphale354@gmail.com>
2026-03-31 16:50:10 +0530

    creating state variable for class component n functional component

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 const User = ({name,location}) => {
+  const [count] = useState(0);
+  const [count2] = useState(1)
   return (
     <div className="user-card">
       <h1>Name : {name} </h1>
       <h2>Location : {location}</h2>
+      <h4>Count : {count}</h4>
+      <h4>Count 2 : {count2}</h4>
     </div>
   )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,12 +5,19 @@ class UserClass extends React.Component{
   constructor(props){
     super(props)
     console.log(props)
+    this.state = {
+      count: 10,
+      count2: 20
+    }
   }
   render(){
     const{name,location} = this.props;
+    const {count,count2} = this.state;
     return <div className="user-card">
       <h1>Name :{name}</h1>
       <h2>Location :{location}</h2>
+      <h4>Count class : {count}</h4>
+      <h4>Count2 class : {count2}</h4>
     </div>
   }
 }
