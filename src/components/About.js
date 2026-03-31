b74c9ad093b38a1f889a3d1f1ecda0a4dbc578d7
Prasad2299 <prasadramphale354@gmail.com>
2026-03-31 18:29:17 +0530

    lifecyce of claas cmoponent using about n userclass commponent

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,39 @@
 import UserClass from "./UserClass";
 import User from "./User";
+import React from "react";
 
-const About = () =>{
-  return <div >
-    <h1>Welcome to About Us</h1>
-    <User name={"Prasad"} location = {"Pune"}></User>
-    <UserClass name={"Prasad class"} location = {"Pune-class"}></UserClass>
-  </div>
+class About extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      // you can put state variable here.
+    }
+    console.log("Parent constructor called!")
+  }
+  componentDidMount(){
+        // API IS CALLED INSDIE IT LIKE USEeffect hook in functional componnet
+
+    console.log("Parent mouted on webpage! ")
+  }
+  render(){
+    console.log("Parent render called! ")
+      return (
+        <div>
+          <h1>Welcome to About Us</h1>
+          <User name={"Prasad"} location={"Pune"}></User>
+          <UserClass name={"Prasad class"} location={"Pune-class"}></UserClass>
+        </div>
+      );
+  }
 }
+
+// const About = () =>{
+//   return <div >
+//     <h1>Welcome to About Us</h1>
+//     <User name={"Prasad"} location = {"Pune"}></User>
+//     <UserClass name={"Prasad class"} location = {"Pune-class"}></UserClass>
+//   </div>
+// }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,31 +1,42 @@
-import React from "react"
+import React from "react";
 
-class UserClass extends React.Component{
-
-  constructor(props){
-    super(props)
-    console.log(props)
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
     this.state = {
       count: 10,
-      count2: 20
-    }
+      count2: 20,
+    };
+    console.log("Child constructor is called!");
   }
-  render(){
-    const{name,location} = this.props;
-    const {count,count2} = this.state;
-    return <div className="user-card">
-      <h1>Name :{name}</h1>
-      <h2>Location :{location}</h2>
-      <h4>Count class : {count}</h4>
-      <button onClick={() => {
-        this.setState({
-          // count:count + 1,
-          count:this.state.count + 1
-        })
-      }}>Count inc</button>
-      <h4>Count2 class : {count2}</h4>
-    </div>
+  componentDidMount() {
+    // API IS CALLED INSDIE IT LIKE USEeffect hook in functional componnet
+    console.log("Child mouted on webpage! ");
+  }
+  render() {
+    const { name, location } = this.props;
+    const { count, count2 } = this.state;
+    console.log("Child render is called!");
+    return (
+      <div className="user-card">
+        <h1>Name :{name}</h1>
+        <h2>Location :{location}</h2>
+        <h4>Count class : {count}</h4>
+        <button
+          onClick={() => {
+            this.setState({
+              // count:count + 1,
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count inc
+        </button>
+        <h4>Count2 class : {count2}</h4>
+      </div>
+    );
   }
 }
 
-export default UserClass
+export default UserClass;
