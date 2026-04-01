ff0fbe3f7d811b6d1d03453a1b075aa921c5d754
Prasad2299 <prasadramphale354@gmail.com>
2026-04-02 00:11:59 +0530

    ADDED LAZY OADING FOR LARGE APPLICATION OPTIMIZATION

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{lazy,Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,6 +7,8 @@ import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
+
 
 const AppLayout = () => {
   return (
@@ -16,6 +18,8 @@ const AppLayout = () => {
     </div>
   );
 };
+
+const Grocery = lazy(()=>import("./components/Grocery"));
 
 const appRouter = createBrowserRouter([
   {
@@ -38,6 +42,10 @@ const appRouter = createBrowserRouter([
       {
         path:"/restaurant/:resId",
         element:<RestaurantMenu/>
+      },
+      {
+        path:"/grocery",
+        element:<Suspense fallback={<h1>LOADING...</h1>}><Grocery/></Suspense>
       }
     ],
   },
---
+++ src/components/Grocery.js
@@ -0,0 +1,11 @@
+const Grocery = () => {
+  return (
+    <div>
+      <h1>
+        WELCOME TO GROCERY PAGE
+      </h1>
+    </div>
+  )
+}
+
+export default Grocery;
