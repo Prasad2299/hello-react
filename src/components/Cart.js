ef7562277c4b2db7ab8d5248b78d335481f5fe93
Prasad2299 <prasadramphale354@gmail.com>
2026-04-19 19:51:08 +0530

    updated code

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,8 @@ import { clearCart } from "../utils/cartSlice.js";
 import Item from "./Item";
 import { useDispatch, useSelector } from "react-redux";
 const Cart = () => {
-  const cart = useSelector((store) => store.cart.items);
+  const cart = useSelector((store) => store.cart.items);//to optimized used liked this ,selecting exact portion from store.
+
   const dispatch = useDispatch()
   console.log("cart item inside cart", cart);
 
@@ -14,6 +15,9 @@ const Cart = () => {
       <h1 className="font-bold">CART</h1>
       <div className="w-6/12 mx-auto">
         <button onClick={handleClearCart} className="text-white bg-black p-2 m-2 rounded-2xl">Clear Cart</button>
+        {
+          cart.length === 0 && <h1>Cart is empty .Please enter ...</h1>
+        }
         <Item item={cart}></Item>
       </div>
     </div>
